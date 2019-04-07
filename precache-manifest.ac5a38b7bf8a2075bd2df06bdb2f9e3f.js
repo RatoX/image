3485393dbe6c6e741adfda3d7f23cc15e217f22c
RatoX <rodrigovieira18@gmail.com>
2019-04-07 17:11:46 +1000

    Updates

---
+++ precache-manifest.ac5a38b7bf8a2075bd2df06bdb2f9e3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7c849ad540c6c2a0455",
+    "url": "/js/about.ad763791.js"
+  },
+  {
+    "revision": "3c63a18df619b4551c0a",
+    "url": "/css/app.9a8be712.css"
+  },
+  {
+    "revision": "3c63a18df619b4551c0a",
+    "url": "/js/app.26fa16e7.js"
+  },
+  {
+    "revision": "2c38e8db5719aa7ce282",
+    "url": "/js/chunk-vendors.00879144.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "25dfc75d716f80441025a4d6a48f3fd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
